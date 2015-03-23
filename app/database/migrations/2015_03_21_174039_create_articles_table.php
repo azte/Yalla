b8dbd3ff49740872b2b18ab2f4382238d380b66c
azte <cuellar.uk@gmail.com>
2015-03-22 20:27:45 -0600

    Cambio de DB

--- app/database/migrations/2015_03_21_174039_create_articles_table.php
+++ app/database/migrations/2015_03_21_174039_create_articles_table.php
@@ -17,7 +17,7 @@ class CreateArticlesTable extends Migration {
         {
             $table->increments('id');
             $table->string('title');
-            $table->string('text');
+            $table->text('text');
             $table->string('author');
 			$table->timestamps();
         });	
